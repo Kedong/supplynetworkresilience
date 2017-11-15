a693f3da7d4ecd3e068ad4f3e75d088fbffdbc58
kedong.kc <kedong.kc@alibaba-inc.com>
2017-11-15 16:10:35 -0600

    regular update

--- supply_network_resilience.R
+++ supply_network_resilience.R
@@ -1,87 +1,84 @@
-######### Section 1. Setup and Simulate networks #########
 library(igraph)
 library(NetSim)
 
-## We simulate a focal company's supply network
-## For three types: random, scale-free, small-world
+######### Section 1. Setup and Simulate networks #########
+source('network_construction.r')
+# network_construction(tier, sup_num_tier, p_edge_tier, same_tier=0, p_edge_same_tier, direction=T, seed=1)
 
-# 1. Random
-n=100
-p=0.05
-# p is the probability for drawing an edge between two arbitrary vertices (G(n,p) graph)
-rdm_1 = erdos.renyi.game(n=n, p, directed=T, type = "gnp")
-plot(rdm_1, vertex.label = NA, vertex.size=3, edge.arrow.size=0.17, xlab = "Random graph model")
-
-# 2. Scale-free
-n=100
-p=0.1
-sf_1 = barabasi.game(n=n, power=p, directed=F) 
-plot(sf_1, vertex.label = NA, vertex.size=3, edge.arrow.size=0.17, xlab = "Scale free model") 
-
-# 3. small-world
-# We use NetSim to generate directed small-world network
-# a regular ring lattice structure with 19 nodes is defined as the starting point
-n=100
-p=0.8
-nei=2
-# p is the rewiring probability
-sw_1 = watts.strogatz.game(dim=1, size=n, nei=1, p=p, loops = FALSE, multiple = FALSE, directed=T) #undirected
-plot(sw_1, vertex.label = NA, vertex.size=3, edge.arrow.size=0.17, xlab = "Small world model")
-
-# 4. Create supply network
-# First, determine how many tiers, without focal company (tier-0)
 set.seed(1)
-tier = 5
-# Second, determine how many suppliers in each tier, including focal company
-sup_num_tier = c(1,round(runif(tier, 10, 20),0))  # uniform distribution
-#edge_p_tier = runif(tier)
-p_edge = 0.35  # used in random graph
-p_same_tier_edge = 0.05
-edge_p_tier = rep(p_edge, tier)
-# Third, construct adjacency matrix
-# Scenario 1. Same weight
-adj_m = matrix(0, sum(sup_num_tier), sum(sup_num_tier))
-same_tier = 0 #0: don't allow same-tier connection
-strt_pos = 1
-for (i in 2:(tier+1)){
-  #adj_temp = matrix(ifelse(runif(sup_num_tier[i-1]*sup_num_tier[i])<=edge_p_tier[i-1],1,0), sup_num_tier[i], sup_num_tier[i-1])
-  rdm_bipar_temp = sample_bipartite(n1=sup_num_tier[i-1], n2=sup_num_tier[i], type="gnp", p=edge_p_tier[i-1], directed=T, mode="in")
-  adj_temp = as_adjacency_matrix(rdm_bipar_temp)[(1+sup_num_tier[i-1]):(sup_num_tier[i-1]+sup_num_tier[i]),(1:sup_num_tier[i-1])]
-  adj_m[(strt_pos+1):(strt_pos+sup_num_tier[i]),((strt_pos-sup_num_tier[i-1]+1):strt_pos)] = as.matrix(adj_temp)
-  if (same_tier) {
-    # add same-tier links
-    for (j in (strt_pos+2):(strt_pos+sup_num_tier[i])){
-      for (k in (strt_pos+1):(j-1)){
-        adj_m[j,k] = ifelse(runif(1)<=p_same_tier_edge,1,0)
-      }
-    }
-  }
-  strt_pos = strt_pos + sup_num_tier[i]
-}
-# Generate vertex names
-sn_1 = graph_from_adjacency_matrix(adj_m)
-V(sn_1)$name = paste0("v",0:(sum(sup_num_tier)-1))
-V(sn_1)$tier = rep(c(0:tier),sup_num_tier)
-#V(sn_1)$color = ifelse(V(sn_1)$name %in% "v0", "red", "blue")
-plot(sn_1, vertex.label = NA, vertex.size=4, edge.arrow.size=0.2, vertex.color=(1+V(sn_1)$tier))
+tier = 4
+sup_num_tier = c(1,round(runif(tier, 3, 6),0))
+p_edge = 0.35
+p_edge_tier = rep(p_edge, tier)  # prob can be different in diff tiers
+same_tier=0
+p_edge_same_tier = 0.05  # also can be different
+direction=T
+
+sn = network_construction(tier, sup_num_tier, p_edge_tier, same_tier=0, p_edge_same_tier, direction=T)
+
+## plot the graph
+sn_1 = sn$sn_ori
+sn_2 = sn$sn_reduced
+
+plot(sn_1, vertex.label = NA, vertex.size=4, edge.arrow.size=0.2, vertex.color=V(sn_1)$color)
 par(oma=c(0, 0, 0, 5))
 legend(par('usr')[2], par('usr')[4], bty='n', xpd=NA, legend=paste("tier", 0:tier), col=categorical_pal(tier+1), pch=16)
 
 deg.dist = degree_distribution(sn_1, cumulative=T)
 plot(x=0:max(degree(sn_1)), y=1-deg.dist, pch=19, cex=1.2, col="orange", xlab="Degree", ylab="Cumulative Frequency")
 
-# need to check random bipartite graph properties
-# Q: is the multi-layer random bipartite still random?
-
-# Q: how to remove links to make the graph scale-free or small-world?
-
-######### Section 2. Resilience in the supply network #########
-# sn_2 is the focal company's extended ego network
-sn_2 = make_ego_graph(sn_1, order=tier, nodes="v0", mode="in")
-plot(sn_2[[1]], vertex.label = NA, vertex.size=4, edge.arrow.size=0.2, vertex.color=(1+V(sn_2[[1]])$tier))
+plot(sn_2, vertex.label = NA, vertex.size=4, edge.arrow.size=0.2, vertex.color=(1+V(sn_2)$tier))
 par(oma=c(0, 0, 0, 5))
 legend(par('usr')[2], par('usr')[4], bty='n', xpd=NA, legend=paste("tier", 0:tier), col=categorical_pal(tier+1), pch=16)
 
+######### Section 2. Dynamic disruptions #########
+# All nodes are with initial health score 1
+# All last-tier suppliers have products 1
+# If the network is healthy, the focal node should receive 1*last-tier-suppliers
+# When disruption happens, the number of products can be discounted at nodes or at routes
+# Let's see how much the focal can receive...
+# "Dynamic" means we allow time to recover...
+# We can set the number of disruption in an examination cycle
 
+exam_t = 10 # the examination cycle
+disruption_times = 3 # how many disruptions happen -- the first always happens at the beginning
+if (disruption_times == 1) {disrupt_t = 1} else { disrupt_t = c(1,sort(sample(2:exam_t, (disruption_times-1))))}
+# disrupt_t is the time points that disruptions happen
+focal_total_prod = numeric(exam_t)
 
-######### Section 3. Searching cost in the supply network #########
+# order and production state
+prod_adj = sn$sn_reduced_adj
+prod_adj[,1] = 1 / sum(prod_adj[,1]) * prod_adj[,1]
+for (i in 2:gorder(sn$sn_reduced)){
+  if (sum(prod_adj[,i])){
+    prod_adj[,i] = sum(prod_adj[i,]) / sum(prod_adj[,i]) * prod_adj[,i]
+  }
+}
+
+prct_node_down = 0.3   # percentage nodes down
+node_down_to_capacity = 0.7   # only a portion of products will go through...
+recovery_node_each_time = 0.1
+
+#prct_edge_down = 0.3
+#edge_down_to_capacity = 0.7
+#recovery_edge_each_time = 0.1
+
+# start the time...
+node_health = rep(1, gorder(sn$sn_reduced))
+for (t in 1:exam_t){
+  if (t %in% disrupt_t){  # means there is a disruption
+    # randomly choose nodes
+    # focal node won't down...
+    unhealth = 1 - (1 - c(1,1*(runif(gorder(sn$sn_reduced)-1) > prct_node_down))) * (1 - node_down_to_capacity)
+    # here the unhealth is totally random -- in reality, the one got hurt may not be hurt again...
+    node_health = pmin(unhealth, node_health)
+  }
+  # check capacity backwards...
+  # last-tier capacity
+  prod_temp_tier = colSums(node_health[which(V(sn$sn_reduced)$tier == tier)] * prod_adj[which(V(sn$sn_reduced)$tier == tier),])
+  for (i in (tier-1):1){
+    prod_temp_tier = colSums(prod_temp_tier[which(V(sn$sn_reduced)$tier == i)] * node_health[which(V(sn$sn_reduced)$tier == i)] * prod_adj[which(V(sn$sn_reduced)$tier == i),] / rowSums(prod_adj[which(V(sn$sn_reduced)$tier == i),]))
+  }
+  focal_total_prod[t] = prod_temp_tier[1]
+  node_health = sapply(node_health + recovery_node_each_time, min, 1)
+}
